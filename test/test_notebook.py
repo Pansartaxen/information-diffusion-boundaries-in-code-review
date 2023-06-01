d64df6773eee54d52bc4697f8280b4f2bf2df615
Pansartaxen <matk20@student.bth.se>
2023-06-01 17:10:28 +0200

    Some updates to test_notebook

--- test/test_notebook.py
+++ test/test_notebook.py
@@ -1,15 +1,35 @@
-import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
+import base64
+import cv2
+import os
+from datetime import timedelta
+import imagehash
 import matplotlib.pyplot as plt
+import nbformat
+import numpy as np
+from nbconvert.preprocessors import ExecutePreprocessor
+from PIL import Image
+import pandas as pd
+import unittest
+import base64
+import cv2
 import numpy as np
 from PIL import Image
-import cv2
-import imagehash
-import base64
-import unittest
+from skimage.metrics import structural_similarity as ssim
+from skimage.transform import resize
 
-class Test_motebook(unittest.TestCase):
-    def execute_notebook(self, notebook_path):
+class TestNotebook(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #self._notebook = self.execute_notebook('notebooks/plot.ipynb')
+        self._notebook = self.execute_notebook('test/mock_notebook.ipynb')
+
+    def execute_notebook(self, notebook_path) -> nbformat.NotebookNode:
+        """
+        Executes the notebook and stores it in
+        the self._notebook for quick access
+        """
+        if not os.path.isfile('data/minimal_paths/microsoft.pickle.bz2'):
+            self.skipTest(f"File 'microsoft.pickle.bz2' not found, skipping the test.")
         with open(notebook_path) as f:
             notebook = nbformat.read(f, as_version=4)
 
@@ -19,18 +39,20 @@ class Test_motebook(unittest.TestCase):
 
         return notebook
 
-    def test_notebook_outputs(self):
-        notebook_path = 'notebooks/plot.ipynb'
+    def test_notebook_outputs(self) -> None:
+        """Evaluates the plot outputed by the notebook"""
+        if not os.path.isfile('data/minimal_paths/microsoft.pickle.bz2'):
+            self.skipTest(f"File 'microsoft.pickle.bz2' not found, skipping the test.")
 
-        notebook = self.execute_notebook(notebook_path)
-
-        last_cell = notebook.cells[-1]
+        last_cell = self._notebook.cells[-1]
+        first_output = last_cell.outputs[0]
 
         if last_cell.outputs:
             first_output = last_cell.outputs[0]
+            print('First output:', first_output)
             data = first_output['data']
 
-            image_disk = cv2.imread('test/Reference_pictures/output.png')
+            image_disk = cv2.imread('test/Reference_pictures/output_test.png')
 
             image_memory_base64 = data['image/png']
 
@@ -50,11 +72,36 @@ class Test_motebook(unittest.TestCase):
             image_disk = np.array(image_disk, dtype=np.uint8)
             image_memory = np.array(image_memory, dtype=np.uint8)
 
+            print(image_disk)
+            print(image_memory)
+
             # Calculate the perceptual hashes
             hash_disk = imagehash.average_hash(Image.fromarray(image_disk))
             hash_memory = imagehash.average_hash(Image.fromarray(image_memory))
 
             self.assertAlmostEqual(hash_disk, hash_memory, delta = 5)
+
+    def test_pickle_read(self) -> None:
+        """Checks if the pickle is read correctly"""
+        if not os.path.isfile('data/minimal_paths/microsoft.pickle.bz2'):
+            self.skipTest(f"File 'microsoft.pickle.bz2' not found, skipping the test.")
+
+        reference = pd.read_pickle('data/minimal_paths/microsoft.pickle.bz2')
+        cell = self._notebook.cells[2]
+        cell_var = cell.outputs[0]['text/plain']
+
+        if cell.cell_type == 'code' and cell.outputs:
+            for output in cell.outputs:
+                if output.output_type == 'execute_result' and 'data' in output:
+                    if 'text/plain' in output.data:
+                        cell_var = output.data['text/plain']
+                        if cell_var == repr(reference):
+                            print('Values are equal')
+                        else:
+                            print('Values are not equal')
+                        return
+
+        self.assertEqual(cell_var, reference)
 
 
 if __name__ == '__main__':
