a7d8dd40c6aca4bebed7b4cd25de8f3cf180e331
Markus-Cedergren <markuscedergren@gmail.com>
2023-06-02 11:43:55 +0200

    renamed tests, added new tests

--- test/test_minimal_paths.py
+++ test/test_minimal_paths.py
@@ -7,54 +7,54 @@ from simulation.minimal_paths import single_source_dijkstra_vertices, single_sou
 
 class MinimalPath(unittest.TestCase):
     cn = CommunicationNetwork({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': 1, 'h2': 2, 'h3': 3})
-    cn1 = CommunicationNetwork({'-1000045392462314428': {-6410414390854871141, -4790071369877151138}},{'-1000045392462314428': datetime.datetime(2020,2,5,12,49,39)})
+    cn1 = CommunicationNetwork({'1': {"participant 1", "participant 2", "participant 3"}},{'1': datetime.datetime(2023,6,1,10,10,10)})
 
-    def test_1(self):
+    def test_shortest_path(self):
         self.assertEqual(single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.SHORTEST, min_timing=0), {'v2': 1, 'v3': 2, 'v4': 3})
 
-    def test_2(self):
+    def test_equal_output_SHORTEST(self):
         result_1 = single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.SHORTEST, min_timing=0)
         result_2 = single_source_dijkstra_hyperedges(MinimalPath.cn, 'v1', DistanceType.SHORTEST, min_timing=0)
         self.assertEqual(result_1, result_2, 'Single-source Dijkstra implementations are not equivalent')
 
-    def test_3(self):
+    def test_equal_output_FASTEST(self):
         result_1 = single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.FASTEST, min_timing=0)
         result_2 = single_source_dijkstra_hyperedges(MinimalPath.cn, 'v1', DistanceType.FASTEST, min_timing=0)
         self.assertEqual(result_1, result_2, 'Single-source Dijkstra implementations are not equivalent')
 
-    def test_4(self):
+    def test_equal_output_FOREMOST(self):
         result_1 = single_source_dijkstra_vertices(MinimalPath.cn, 'v1', DistanceType.FOREMOST, min_timing=0)
         result_2 = single_source_dijkstra_hyperedges(MinimalPath.cn, 'v1', DistanceType.FOREMOST, min_timing=0)
         self.assertEqual(result_1, result_2, 'Single-source Dijkstra implementations are not equivalent')
 
 
-    def test_5(self):
-        result = single_source_dijkstra_hyperedges(MinimalPath.cn1,-6410414390854871141, DistanceType.SHORTEST, datetime.datetime(1,1,1,0,0))
-        expected_res = {-4790071369877151138: 1}
+    def test_hedges_SHORTEST_path(self):
+        result = single_source_dijkstra_hyperedges(MinimalPath.cn1,"participant 1", DistanceType.SHORTEST, datetime.datetime(1,1,1,0,0))
+        expected_res = {"participant 3" : 1,"participant 2": 1}
         self.assertEqual(result, expected_res)
 
-    def test_6(self):
-        result = single_source_dijkstra_vertices(MinimalPath.cn1,-6410414390854871141, DistanceType.SHORTEST, datetime.datetime(1,1,1,0,0))
-        expected_res = {-4790071369877151138: 1}
+    def test_vertex_SHORTEST_path(self):
+        result = single_source_dijkstra_vertices(MinimalPath.cn1,"participant 1", DistanceType.SHORTEST, datetime.datetime(1,1,1,0,0))
+        expected_res = {'participant 2': 1, 'participant 3': 1}
         self.assertEqual(result, expected_res)
 
 
-    def test_7(self):
-        result = single_source_dijkstra_vertices(MinimalPath.cn1,-6410414390854871141, DistanceType.FASTEST, datetime.datetime(2020,2,5,12,49,39))
-        expected_res = {-4790071369877151138: datetime.timedelta(0)}
+    def test_vertex_FASTEST_path(self):
+        result = single_source_dijkstra_vertices(MinimalPath.cn1,"participant 1", DistanceType.FASTEST, datetime.datetime(2023,6,1,10,10,10))
+        expected_res = {"participant 3" : datetime.timedelta(0),"participant 2": datetime.timedelta(0)}
         self.assertEqual(result, expected_res)
 
-    def test_8(self):
-        result = single_source_dijkstra_hyperedges(MinimalPath.cn1,-6410414390854871141, DistanceType.FASTEST, datetime.datetime(2020,2,5,12,49,39))
-        expected_res = {-4790071369877151138: datetime.timedelta(0)}
+    def test_hedges_FASTEST_path(self):
+        result = single_source_dijkstra_hyperedges(MinimalPath.cn1,"participant 1", DistanceType.FASTEST, datetime.datetime(2023,6,1,10,10,10))
+        expected_res = {"participant 3": datetime.timedelta(0), "participant 2": datetime.timedelta(0)}
         self.assertEqual(result, expected_res)
 
-    def test_9(self):
-        result = single_source_dijkstra_hyperedges(MinimalPath.cn1,-6410414390854871141, DistanceType.FOREMOST, datetime.datetime(2020,2,5,12,49,39))
-        expected_res = {-4790071369877151138: datetime.timedelta(0)}
+    def test_hedges_FOREMOST_path(self):
+        result = single_source_dijkstra_hyperedges(MinimalPath.cn1,"participant 1", DistanceType.FOREMOST, datetime.datetime(2020,2,5,12,49,39))
+        expected_res = {"participant 3" :datetime.datetime(2023,6,1,10,10,10),"participant 2": datetime.datetime(2023,6,1,10,10,10)}
         self.assertEqual(result, expected_res)
 
-    def test_10(self):
-        result = single_source_dijkstra_vertices(MinimalPath.cn1,-6410414390854871141, DistanceType.FOREMOST, datetime.datetime(2020,2,5,12,49,39))
-        expected_res = {-4790071369877151138: datetime.timedelta(0)}
-        self.assertEqual(result,expected_res)
+    def test_vertex_FOREMOST_path(self):
+        result = single_source_dijkstra_vertices(MinimalPath.cn1,"participant 1", DistanceType.FOREMOST, datetime.datetime(2020,2,5,12,49,39))
+        expected_res = {"participant 3" :datetime.datetime(2023,6,1,10,10,10),"participant 2": datetime.datetime(2023,6,1,10,10,10)}
+        self.assertEqual(result, expected_res)
